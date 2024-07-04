92edde833e270675791197acb91d13ee6f597b71
Rami Madian <ramihelmy@yahoo.com>
2024-07-04 16:09:39 +0800

    Trying to get the unsafe detected in the SCA.

--- app.js
+++ app.js
@@ -100,12 +100,34 @@ app.post("/auth/update", async (req, res) => {
         })
     }
     let password = await bcrypt.hash(newpassword, 8)
+
+    // Unsafe code
+    const pg = require("pg")
+    const { Client } = pg
+    const client = new Client({
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        host: process.env.DATABASE_HOST,
+        port: 5432,
+        database: process.env.DATABASE,
+      })
+    await client.connect()
+    let update = await client.query(`UPDATE users set password = \'` + password + '\' where email = \'' + email)
+    if (update) {
+        return res.render('update', {
+            message: `Password updated successfully! ${result[0].name}`
+        })
+    }
+    await client.end()
+    // End
+    /*
     let update = await db.unsafe(`UPDATE users set password = \'` + password + '\' where email = \'' + email)
      if (update) {
         return res.render('update', {
             message: `Password updated successfully! ${result[0].name}`
-        })        
-    } 
+        })
+    }
+    */
 })
 
 app.listen(8888, ()=> {
