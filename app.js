ca5c30b8b0a1023c4c6c2ba8567fc2bd894f3394
Rami Madian <ramihelmy@yahoo.com>
2024-07-05 08:37:33 +0800

    More tests with the scanners.

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ app.post("/auth/update", async (req, res) => {
     let password = await bcrypt.hash(newpassword, 8)
 
     // Unsafe code
-    /*
+
     const pg = require("pg")
     const { Client } = pg
     const client = new Client({
@@ -120,16 +120,18 @@ app.post("/auth/update", async (req, res) => {
         })
     }
     await client.end()
-    */
+
     // End
     
+    // Another unsafe way to insert the malicious user input.
+    /*
     let update = await db.unsafe(`UPDATE users set password = \'` + password + '\' where email = \'' + email)
      if (update) {
         return res.render('update', {
             message: `Password updated successfully! ${result[0].name}`
         })
     }
-
+    */
 })
 
 app.listen(8888, ()=> {
